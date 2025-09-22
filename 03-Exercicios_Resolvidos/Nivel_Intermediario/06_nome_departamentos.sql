f3d977bc2947524869edac9c9ca25d7a1fea1168
danielduartt <dev.danie.duarte@gmail.com>
2025-09-22 17:55:11 -0300

    fix: add respostas dos ex

--- 03-Exercicios_Resolvidos/Nivel_Intermediario/06_nome_departamentos.sql
+++ 03-Exercicios_Resolvidos/Nivel_Intermediario/06_nome_departamentos.sql
@@ -2,4 +2,11 @@
 --Escreva uma query que liste o  nome completo do funcionário e o nome do 
 -- seu respectivo departamento. Use um JOIN.
 -- =================================================================================
+SELECT 
+    CONCAT(f.nome, ' ', f.sobrenome) AS nome_completo, 
+    d.nome_depto
+FROM rh.funcionarios f
+INNER JOIN rh.departamentos d 
+    ON f.id_depto = d.id_depto
+ORDER BY nome_completo ASC;
 
